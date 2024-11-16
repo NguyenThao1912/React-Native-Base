1865e8c3d44b5b6645e3124c1ac9646abc670de1
Your Name <you@example.com>
2024-11-16 13:31:16 +0700

    feat: update lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,20 +2,22 @@ import pluginJs from '@eslint/js'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
+import expo from 'eslint-config-expo'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     plugins: {
-      'simple-import-sort': simpleImportSort
+      'simple-import-sort': simpleImportSort,
+      'expo': expo
     },
     ignores: ['node_modules/*'],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/ban-types': 'off',
-      'no-var': 0,
+      "no-unused-vars": "warn",
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
       "react/jsx-uses-react": "off",
@@ -26,5 +28,6 @@ export default [
   },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended
+  ...tseslint.configs.recommended,
+
 ]
